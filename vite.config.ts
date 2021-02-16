e208836bb5ac35f3070a2908f7551558b1cf5b74
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-02-16 20:46:02 -0300

    ✨ setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import { VitePWA } from 'vite-plugin-pwa'
 
+import pkg from './package.json'
+
+process.env.VITE_APP_VERSION = pkg.version
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false
+  },
   plugins: [
     vue(),
     ...WindiCSS(),
+    VitePWA({
+      minify: false,
+      manifest: {
+        name: 'EchoBravo - Investing Calc',
+        short_name: 'Investing Calc',
+        orientation: 'portrait',
+        description: 'Calculadora de juros compostos',
+        theme_color: '#66d59a',
+        lang: 'pt-BR',
+      },
+      workbox: {
+        cacheId: `bravo:${pkg.version}`,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+        sourcemap: false
+      }
+    }),
   ]
 })
