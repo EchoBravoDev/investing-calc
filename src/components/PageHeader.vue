9b137ece87a7e98f4e2918168add25232c670a71
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-03-05 19:58:31 -0300

    ✨ connection detect

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,21 +1,40 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import IconCalc from './icons/Calc.vue'
+import CloudOff from './icons/CloudOff.vue'
 
 export default defineComponent({
   name: 'PageHeader',
   components: {
-    IconCalc
+    IconCalc,
+    CloudOff
+  },
+  setup () {
+    const online = ref(navigator.onLine)
+
+    const buildConnChange = (val: boolean) => {
+      return () => online.value = val
+    }
+
+    window.addEventListener('online', buildConnChange(true));
+    window.addEventListener('offline', buildConnChange(false));
+
+    return {
+      online
+    }
   }
 })
 </script>
 
 <template>
-  <header class="page-header">
+  <header class="page-header" :class="{ 'is-offline': !online }">
     <div class="container">
       <div class="flex items-center justify-center">
         <h1>
-          <IconCalc /> EchoBravo Investing
+          <IconCalc v-if="online" />
+          <CloudOff v-else />
+
+          EchoBravo Invest
         </h1>
       </div>
     </div>
