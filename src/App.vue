1f0142fb0a63e6c4b92ec0e1b073daaecb784006
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-03-05 19:51:43 -0300

    ✨ update detection

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <PageHeader />
   <main class="container">
+    <UpdateDetection />
     <PageForm />
     <Results />
     <PageFooter />
@@ -12,11 +13,13 @@ import { defineComponent } from 'vue'
 import PageHeader from './components/PageHeader.vue'
 import PageFooter from './components/PageFooter.vue'
 import PageForm from './components/Form.vue'
+import UpdateDetection from './components/UpdateDetection.vue'
 import Results from './components/Results.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    UpdateDetection,
     PageForm,
     PageHeader,
     PageFooter,
---
+++ src/components/UpdateDetection.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'update-detection',
+  setup () {
+    const hasUpdate = ref(false)
+
+    document.addEventListener('sw:update', () => {
+      hasUpdate.value = true
+    })
+
+    return {
+      update :() => window.location.reload(),
+      hasUpdate
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="grid mt-5" v-if="hasUpdate">
+    <button @click="update" class="is-alert">
+     Uma nova versão esta disponível. <br />
+     <strong>Toque</strong> ou <strong>clique</strong> aqui para atualizar.
+    </button>
+  </div>
+</template>
